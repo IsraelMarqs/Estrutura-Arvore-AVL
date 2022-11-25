cdc7c73e327cb09670cb9ff2e9affc72fc4949c6
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-11-25 09:58:36 -0300

    Teste inicial

---
+++ Heapsort.dart
@@ -0,0 +1,15 @@
+
+class Heapsort{
+  var heap = [];
+
+  Heapsort(){
+    heap.add(null);
+  }
+  void Insert(int number){
+    heap.add(number);
+  }
+
+  void RemoveSmallest(){
+
+  }
+}
---
+++ Main.dart
@@ -0,0 +1,7 @@
+import 'Heapsort.dart';
+
+void main(){
+  Heapsort heapsort = new Heapsort();
+  heapsort.Insert(5);
+  print(heapsort.heap);
+}
