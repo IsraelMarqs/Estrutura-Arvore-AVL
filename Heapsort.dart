5e41e16a0fc74589192b4a70bf065f15d2c4a497
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-11-25 22:21:58 -0300

    Remove Smallest e Altura

--- Heapsort.dart
+++ Heapsort.dart
@@ -1,15 +1,37 @@
+import 'dart:math';
 
 class Heapsort{
   var heap = [];
+  int size() => heap.length-1;
+  int altura() => (log(size()) ~/ log(2));
 
   Heapsort(){
     heap.add(null);
   }
-  void Insert(int number){
+  void insert(int number){
     heap.add(number);
+    for (var x=1; x<=altura(); x++){
+
+    }
   }
 
-  void RemoveSmallest(){
-
+  void removeSmallest(){
+    heap[1] = heap.last;
+    heap.removeLast();
+    for (var x=1; x<=altura(); x++){
+      if(heap[x*2] < heap[x*2+1]){
+        if(heap[x]>heap[x*2]){
+          int aux = heap[x];
+          heap[x] = heap[x*2];
+          heap[x*2] = aux;
+        }
+      }else{
+        if(heap[x] > heap[x*2+1]){
+          int aux = heap[x];
+          heap[x] = heap[x*2+1];
+          heap[x*2+1] = aux;
+        }
+      }
+    }
   }
 }
--- Main.dart
+++ Main.dart
@@ -2,6 +2,15 @@ import 'Heapsort.dart';
 
 void main(){
   Heapsort heapsort = new Heapsort();
-  heapsort.Insert(5);
+  heapsort.insert(2);
+  heapsort.insert(3);
+  heapsort.insert(4);
+  heapsort.insert(5);
+  heapsort.insert(6);
+  heapsort.insert(7);
+  heapsort.insert(8);
   print(heapsort.heap);
+  heapsort.removeSmallest();
+  print(heapsort.heap);
+
 }
