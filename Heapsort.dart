32681b9987e726a56105e54a4ffb0d398227bc5d
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-12-09 20:48:29 -0300

    Sort func
    Co-Authored-By: Israel Marques de Oliveira <90154937+IsraelMarqs@users.noreply.github.com>

--- Heapsort.dart
+++ Heapsort.dart
@@ -10,29 +10,85 @@ class Heapsort{
   }
   void insert(int number){
     heap.add(number);
+    int position = heap.indexOf(heap.last);
     for (var x=1; x<=altura(); x++){
-
+      if(heap[(position/2).floor()]>heap[position]){
+        int aux = heap[position];
+        heap[position]=heap[(position/2).floor()];
+        heap[(position/2).floor()]=aux;
+        position=(position/2).floor();
+      }
     }
   }
 
   void removeSmallest(){
     heap[1] = heap.last;
-    heap.removeLast();
-    int position=1;
-    for (var x=1; position<=altura(); position++){
-      if(heap[position*2] < heap[position*2+1]){
-        if(heap[position]>heap[position*2]){
-          int aux = heap[position];
-          heap[position] = heap[position*2];
-          heap[position*2] = aux;
+    int ind = 1;
+    try{
+      for (var x=1; x<=altura(); x++){
+        if(heap[ind*2] < heap[ind*2+1]){
+          if(heap[ind]>heap[ind*2]){
+            int aux = heap[ind];
+            heap[ind] = heap[ind*2];
+            heap[ind*2] = aux;
+            ind = ind*2;
+            continue;
+          }
+          break;
         }
-      }else{
-        if(heap[position] > heap[position*2+1]){
-          int aux = heap[position];
-          heap[position] = heap[position*2+1];
-          heap[position*2+1] = aux;
+        if(heap[ind*2] > heap[ind*2+1]){
+          if(heap[ind] > heap[ind*2+1]){
+            int aux = heap[ind];
+            heap[ind] = heap[ind*2+1];
+            heap[ind*2+1] = aux;
+            ind = ind*2+1;
+            continue;
+          }
+          break;
         }
       }
+    }catch(e){}
+    heap.removeLast();
+  }
+  List removeSmall(var heap){
+    heap[1] = heap.last;
+    int ind = 1;
+    try{
+      for (var x=1; x<=altura(); x++){
+        if(heap[ind*2] < heap[ind*2+1]){
+          if(heap[ind]>heap[ind*2]){
+            int aux = heap[ind];
+            heap[ind] = heap[ind*2];
+            heap[ind*2] = aux;
+            ind = ind*2;
+            continue;
+          }
+          break;
+        }
+        if(heap[ind*2] > heap[ind*2+1]){
+          if(heap[ind] > heap[ind*2+1]){
+            int aux = heap[ind];
+            heap[ind] = heap[ind*2+1];
+            heap[ind*2+1] = aux;
+            ind = ind*2+1;
+            continue;
+          }
+          break;
+        }
+      }
+    }catch(e){}
+    heap.removeLast();
+     return heap;
+  }
+
+  List sort(){
+    List listaord = [];
+    int ind = size();
+    var heapaux = heap;
+    for(var x=0;x<ind;x++){
+      listaord.add(heapaux[1]);
+      heapaux =  removeSmall(heapaux);
     }
+    return listaord;
   }
 }
--- Main.dart
+++ Main.dart
@@ -2,18 +2,10 @@ import 'Heapsort.dart';
 
 void main(){
   Heapsort heapsort = new Heapsort();
-  heapsort.insert(2);
   heapsort.insert(4);
   heapsort.insert(3);
-  heapsort.insert(5);
-  heapsort.insert(6);
-  heapsort.insert(7);
-  heapsort.insert(8);
-  heapsort.insert(9);
+  heapsort.insert(1);
   print(heapsort.heap);
-  heapsort.removeSmallest();
-  print(heapsort.heap);
-  heapsort.removeSmallest();
-  print(heapsort.heap);
+  print(heapsort.sort());
 
 }
