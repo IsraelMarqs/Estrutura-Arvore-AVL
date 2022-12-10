9454718b01d3f35177bba55f91f4542daa0b62f3
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-12-10 20:43:42 -0300

    Apresentação
    Co-Authored-By: Israel Marques de Oliveira <90154937+IsraelMarqs@users.noreply.github.com>

--- Main.dart
+++ Main.dart
@@ -1,11 +1,44 @@
+import 'dart:io';
+
 import 'Heapsort.dart';
 
 void main(){
   Heapsort heapsort = new Heapsort();
-  heapsort.insert(4);
-  heapsort.insert(3);
-  heapsort.insert(1);
-  print(heapsort.heap);
-  print(heapsort.sort());
+  while(1>0){
+    print("Escolha uma função:");
+    print("'1':para insert   '2':para remove smalltest   '3':para por em ordem   '4':para clear");
+    String? func=stdin.readLineSync();
+    switch(func){
+      case"1":{
+        print("Insira um número");
+        int num = int.parse(stdin.readLineSync().toString());
+        heapsort.insert(num);
+        print(heapsort.heap);
+        print("");
+      }
+      break;
+      case"2":{
+        heapsort.removeSmallest();
+        print(heapsort.heap);
+        print("");
+      }
+      break;
+      case"3":{
+        print(heapsort.sort());
+        print("");
+      }
+      break;
+      case"4":{
+        heapsort = new Heapsort();
+        print(heapsort.heap);
+        print("");
+      }
+      break;
+      default:{
+        print("Programa será encerrado");
+        return;
+      }
+    }
+  }
 
 }
